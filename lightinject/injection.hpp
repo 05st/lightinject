000a10f623369200513365b18d2c2c27905457ff
Sami Timsina <samitimsina05@gmail.com>
2020-05-11 21:15:26 -0500

    ntCreateThreadEx injection

--- lightinject/injection.hpp
+++ lightinject/injection.hpp
@@ -2,6 +2,7 @@
 
 #define CREATE_THREAD_ACCESS (PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ)
 
+// CreateRemoteThread Injection
 BOOL createRemoteThread(DWORD processId, LPCSTR dllPath) {
 	if (!processId) return FALSE;
 
@@ -23,6 +24,93 @@ BOOL createRemoteThread(DWORD processId, LPCSTR dllPath) {
 	
 	if (remoteString != NULL) VirtualFreeEx(process, remoteString, 0, MEM_RELEASE);
 	if (thread != NULL) CloseHandle(thread);
+	if (process != NULL) CloseHandle(process);
+
+	return TRUE;
+}
+
+// NtCreateThreadEx Injection
+struct NtCreateThreadExBuffer {
+	ULONG Size;
+	ULONG Unknown1;
+	ULONG Unknown2;
+	PULONG Unknown3;
+	ULONG Unknown4;
+	ULONG Unknown5;
+	ULONG Unknown6;
+	PULONG Unknown7;
+	ULONG Unknown8;
+};
+
+typedef NTSTATUS(WINAPI* LPFUN_NtCreateThreadEx)(
+	OUT PHANDLE hThread,
+	IN ACCESS_MASK DesiredAccess,
+	IN LPVOID ObjectAttributes,
+	IN HANDLE ProcessHandle,
+	IN LPTHREAD_START_ROUTINE lpStartAddress,
+	IN LPVOID lpParameter,
+	IN BOOL CreateSuspended,
+	IN ULONG StackZeroBits,
+	IN ULONG SizeOfStackCommit,
+	IN ULONG SizeOfStackReserve,
+	OUT LPVOID lpBytesBuffer
+);
+
+BOOL ntCreateThreadEx(DWORD processId, LPCSTR dllPath) {
+	if (!processId) return FALSE;
+
+	LPVOID remoteString;
+	HANDLE remoteThread = NULL;
+	HANDLE process = OpenProcess(CREATE_THREAD_ACCESS, FALSE, processId);
+
+	if (!process) {
+		std::cout << "Failed to open process (PID: " + std::to_string(processId) + ")." << std::endl;
+		return FALSE;
+	}
+
+	remoteString = (LPVOID)VirtualAllocEx(process, NULL, strlen(dllPath) + 1, MEM_COMMIT, PAGE_READWRITE);
+	WriteProcessMemory(process, remoteString, (LPVOID)dllPath, strlen(dllPath) + 1, NULL);
+
+	PTHREAD_START_ROUTINE threadRoutine = (PTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+	PTHREAD_START_ROUTINE ntCreateThreadExAddress = (PTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle("ntdll.dll"), "NtCreateThreadEx");
+
+	NtCreateThreadExBuffer buffer;
+	memset(&buffer, 0, sizeof(NtCreateThreadExBuffer));
+
+	LARGE_INTEGER dwTmp1 = { 0 };
+	LARGE_INTEGER dwTmp2 = { 0 };
+
+	if (ntCreateThreadExAddress) {
+		buffer.Size = sizeof(struct NtCreateThreadExBuffer);
+		buffer.Unknown1 = 0x10003;
+		buffer.Unknown2 = 0x8;
+		buffer.Unknown3 = (DWORD*)&dwTmp2;
+		buffer.Unknown4 = 0;
+		buffer.Unknown5 = 0x10004;
+		buffer.Unknown6 = 4;
+		buffer.Unknown7 = (DWORD*)&dwTmp1;
+		buffer.Unknown8 = 0;
+
+		LPFUN_NtCreateThreadEx funNtCreateThreadEx = (LPFUN_NtCreateThreadEx)ntCreateThreadExAddress;
+		NTSTATUS status = funNtCreateThreadEx(
+			&remoteThread,
+			0x1FFFFF,
+			NULL,
+			process,
+			threadRoutine,
+			(LPVOID)remoteString,
+			FALSE,
+			NULL,
+			NULL,
+			NULL,
+			NULL
+		);
+
+		WaitForSingleObject(remoteThread, INFINITE);
+	}
+
+	if (remoteString != NULL) VirtualFreeEx(process, remoteString, 0, MEM_RELEASE);
+	if (remoteThread != NULL) CloseHandle(remoteThread);
 	if (process != NULL) CloseHandle(process);
 
 	return TRUE;
--- lightinject/main.cpp
+++ lightinject/main.cpp
@@ -8,7 +8,7 @@
 bool validateArguments(int argc, char** argv) {
 	if (argc > 4 || argc < 4) {
 		std::cout << "Invalid usage of LightInject." << std::endl << std::endl;
-		std::cout << "All execution methods: " << std::endl << "(1) CreateRemoteThread" << std::endl << std::endl;
+		std::cout << "All execution methods: " << std::endl << "(1) CreateRemoteThread\n(2) NtCreateThreadEx" << std::endl << std::endl;
 		std::cout << "Example usage: " << std::endl << ".\\lightinject.exe processName.exe \"C:\\example directory\\example.dll 1\"" << std::endl;
 		std::cout << "The example above will inject example.dll into processName.exe using CreateRemoteThread injection (method 1)." << std::endl << std::endl;
 		return false;
@@ -22,7 +22,8 @@ bool validateArguments(int argc, char** argv) {
 			std::cout << "Invalid dll path. Make sure it exists." << std::endl;
 			return false;
 		}*/
-		if (std::stoi(argv[3]) != 1) {
+		short injectionType = std::stoi(argv[3]);
+		if (injectionType < 1 || injectionType > 2) {
 			std::cout << "Invalid injection method. Run without any arguments to see injection methods." << std::endl;
 			return false;
 		}
@@ -34,9 +35,14 @@ int main(int argc, char** argv) {
 	if (validateArguments(argc, argv) == false) return -1;
 
 	short injectionType = std::stoi(argv[3]);
+	DWORD processId = getProcessId(argv[1]);
 	if (injectionType == 1) {
-		if (createRemoteThread(getProcessId(argv[1]), argv[2]) == false) {
-			// Message is already outputted by process.hpp, so exit with return code -1.
+		if (createRemoteThread(processId, argv[2]) == false) {
+			return -1;
+		}
+	}
+	else if (injectionType == 2) {
+		if (ntCreateThreadEx(processId, argv[2]) == false) {
 			return -1;
 		}
 	}
