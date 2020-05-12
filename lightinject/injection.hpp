99a05a1856e168873fbe06a5748bd3fee74ec582
Sami Timsina <samitimsina05@gmail.com>
2020-05-11 20:02:29 -0500

    createRemoteThread injection

---
+++ lightinject/injection.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <Windows.h>
+
+#define CREATE_THREAD_ACCESS (PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ)
+
+BOOL createRemoteThread(DWORD processId, LPCSTR dllPath) {
+	if (!processId) return FALSE;
+
+	LPVOID loadLibAddress, remoteString;
+	HANDLE process = OpenProcess(CREATE_THREAD_ACCESS, FALSE, processId);
+
+	if (!process) {
+		std::cout << "Failed to open process (PID: " + std::to_string(processId) + ")." << std::endl;
+		return FALSE;
+	}
+
+	loadLibAddress = (LPVOID)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+	remoteString = (LPVOID)VirtualAllocEx(process, NULL, strlen(dllPath) + 1, MEM_COMMIT, PAGE_READWRITE);
+
+	WriteProcessMemory(process, remoteString, (LPVOID)dllPath, strlen(dllPath) + 1, NULL);
+	CreateRemoteThread(process, NULL, NULL, (LPTHREAD_START_ROUTINE)loadLibAddress, remoteString, NULL, NULL);
+
+	CloseHandle(process);
+	return TRUE;
+}
--- lightinject/main.cpp
+++ lightinject/main.cpp
@@ -1,11 +1,17 @@
 #pragma once
 #include <iostream>
+#include <string>
 #include <Shlwapi.h>
+
 #include "process.hpp"
+#include "injection.hpp"
 
 bool validateArguments(int argc, char** argv) {
-	if (argc > 3 || argc < 3) {
-		std::cout << "Invalid usage of LightInject. Correct usage: " << std::endl << ".\lightinject.exe processName.exe C:\\directory\\example.dll" << std::endl;
+	if (argc > 4 || argc < 4) {
+		std::cout << "Invalid usage of LightInject." << std::endl << std::endl;
+		std::cout << "All execution methods: " << std::endl << "(1) CreateRemoteThread" << std::endl << std::endl;
+		std::cout << "Example usage: " << std::endl << ".\\lightinject.exe processName.exe \"C:\\example directory\\example.dll 1\"" << std::endl;
+		std::cout << "The example above will inject example.dll into processName.exe using CreateRemoteThread injection (method 1)." << std::endl << std::endl;
 		return false;
 	}
 	else {
@@ -13,8 +19,12 @@ bool validateArguments(int argc, char** argv) {
 			std::cout << "The specified process is not running." << std::endl;
 			return false;
 		}
-		if (PathFileExists(argv[2]) == false) {
+		/*if (PathFileExists(argv[2]) == false) {
 			std::cout << "Invalid dll path. Make sure it exists." << std::endl;
+			return false;
+		}*/
+		if (std::stoi(argv[3]) != 1) {
+			std::cout << "Invalid injection method. Run without any arguments to see injection methods." << std::endl;
 			return false;
 		}
 	}
@@ -23,6 +33,14 @@ bool validateArguments(int argc, char** argv) {
 
 int main(int argc, char** argv) {
 	if (validateArguments(argc, argv) == false) return -1;
+
+	short injectionType = std::stoi(argv[3]);
+	if (injectionType == 1) {
+		if (createRemoteThread(getProcessId(argv[1]), argv[2]) == false) {
+			// Message is already outputted by process.hpp, so exit with return code -1.
+			return -1;
+		}
+	}
 
 	return 0;
 }
